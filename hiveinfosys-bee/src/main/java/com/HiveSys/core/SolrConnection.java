5e18ca08ceb2aee7cd66d7d137a9be239182e1ef
Swoorup Joshi <swoorupj@gmail.com>
2015-05-12 20:18:41 +0800

    Solrj native upload fixed. Deleted the Http upload method

--- hiveinfosys-bee/src/main/java/com/HiveSys/core/SolrConnection.java
+++ hiveinfosys-bee/src/main/java/com/HiveSys/core/SolrConnection.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
 
+import javax.activation.MimetypesFileTypeMap;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -14,107 +16,86 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.SolrRequest.METHOD;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrDocumentList;
-
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.util.ContentStreamBase;
+import org.apache.solr.common.util.ContentStreamBase.FileStream;
+import org.apache.solr.common.util.NamedList;
 
 public class SolrConnection {
-	private SolrClient mSolr = null;
+	private HttpSolrClient mHttpSolrClient = null;
 	private String mUrl = "";
-	
-	SolrConnection()
-	{
+
+	SolrConnection() {
 	}
-	
-	public void connect(String serverUrl)
-	{
+
+	public void connect(String serverUrl) {
 		mUrl = serverUrl;
-		mSolr = new HttpSolrClient(mUrl);
+		mHttpSolrClient = new HttpSolrClient(mUrl);
+		
+		// this fixes all the missing strean http header issues. Eg: Content.Length = null causing stream_size=null 
+		mHttpSolrClient.setUseMultiPartPost(true);
 	}
-	
-	public void close()
-	{
-		try {
-			mSolr.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.exit(1);
-		}
+
+	public void close() {
+
 	}
-	
-	public SolrDocumentList query(String queryString) throws SolrServerException, IOException 
-	{
+
+	public SolrDocumentList query(String queryString)
+			throws SolrServerException, IOException {
 		SolrQuery parameters = new SolrQuery();
 		parameters.set("q", queryString);
 		QueryResponse query = null;
-			query = mSolr.query(parameters);
-			return query.getResults();
+		query = mHttpSolrClient.query(parameters);
+		return query.getResults();
 	}
-	
-	public SolrClient getSolrContext() 
-	{
-		return this.mSolr;
+
+	public SolrClient getHttpSolrClient() {
+		return this.mHttpSolrClient;
 	}
-	
-	@SuppressWarnings("deprecation")
-	public void indexFiles(String filename) throws IOException
-	
-	{
-		 HttpPost method;
-		 String URLfilename = URLEncoder.encode(filename, "UTF-8");
-		 method = new HttpPost(this.mUrl + "update/extract?literal.id=" + URLfilename + "&commit=true");
 
-		    MultipartEntity me = new MultipartEntity();
-		    FileInputStream fs = new FileInputStream(new File(filename));
-		    
-		    me.addPart("myfile", new InputStreamBody(fs, filename));
-
-		    method.setEntity(me);
-		    //method.setHeader("Content-Type", "multipart/form-data");
-		    HttpClient httpClient = new DefaultHttpClient();
-		    HttpResponse hr = httpClient.execute(method);
-		    System.out.println(hr);
-		
-		/*
-		SimplePostTool post = new SimplePostTool();
-		
-		String id = filename.substring(filename.lastIndexOf('/')+1);
+	public void indexFiles(String filename) throws IOException {
+		String id = filename.substring(filename.lastIndexOf('/') + 1);
 		File fd = new File(filename);
 		MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
-        String mimeType = mimeTypesMap.getContentType(fd);
-		 ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract/");
-		 
-		 //req.addFile(fd, mimeType);
-		 ContentStreamBase.FileStream fs = new FileStream(new File(filename));
-		 req.addContentStream(fs);
-		 ModifiableSolrParams out = new ModifiableSolrParams();
-		 try {
-			FieldUtils.writeField(out, "literals.stream_size", 23);
-			req.setParams(out);
-		} catch (IllegalAccessException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		 //req.setParam("literal.id", filename);
-		 req.setParam("extractOnly", "true");
-		 System.out.println("Request to solr: \n" + fs.getName() + fs.getSize() + "\n" + req.getResponseParser());
-		 //req.process(mSolr);
-		 //req.setAction(AbstractUpdateRequest.ACTION.COMMIT, true, true);
-		 NamedList<Object> result;
+		String mimeType = mimeTypesMap.getContentType(fd);
+		ContentStreamUpdateRequest req = new ContentStreamUpdateRequest(
+				"/update/extract");
+
+		//req.addFile(fd, mimeType);
+		ContentStreamBase.FileStream fs = new FileStream(fd);
+		req.addContentStream(fs);
+		//req.setMethod(METHOD.POST);
+		ModifiableSolrParams out = new ModifiableSolrParams();
+		req.setParam("resource.name", filename);
+		req.setParam("literal.id", filename);
+		req.setParam("extractOnly", "true");
+		System.out.println("Request to solr: \n" + "\tName: " + fs.getName()
+				+ "\n" + "\tFile Size: " + fs.getSize() + "\n"
+				+ "\tContent Type: " + fs.getContentType() + "\n"
+				);
+
+		// req.setAction(AbstractUpdateRequest.ACTION.COMMIT, true, true);
+		UpdateResponse result;
 		try {
-			result = this.mSolr.request(req);
-			System.out.println("Response from Solr: ");
-			System.out.println(result);
-			assert result != null : "Can't upload the damn file";
+			
+			result = req.process(mHttpSolrClient);
+			mHttpSolrClient.commit();
+			//System.out.println("Response from Solr: ");
+			//System.out.println(result.getResponse());
+
 		} catch (SolrServerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 */
+
 	}
-	
+
 	// return the default instance of the client
 	public static SolrConnection getDefault() {
 		return StaticSolrConnection.DEFAULT;
