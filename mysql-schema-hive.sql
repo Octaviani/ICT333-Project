30e00fbf0abfe8fd3375e65d8fa7e104aec195b9
Swoorup Joshi <swoorupj@gmail.com>
2015-06-06 19:58:55 +0800

    Added Create and database

--- mysql-schema-hive.sql
+++ mysql-schema-hive.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `Annotation`
 --
 
+
+CREATE DATABASE IF NOT EXISTS Hive;
+USE Hive;
 DROP TABLE IF EXISTS `Annotation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
